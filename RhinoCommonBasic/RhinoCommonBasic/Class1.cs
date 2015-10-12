0a7565627b11b8601ad14c31972d0b3a5f316093
kurema <kurema_makoto_software@yahoo.co.jp>
2015-10-13 03:23:53 +0900

    Add RhinoCommonBasic

---
+++ RhinoCommonBasic/RhinoCommonBasic/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Rhino;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino.Collections;
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace RhinoCommonBasic
+{
+    public class Class1
+    {
+    }
+}
